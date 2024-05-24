c9adf257eb995ca7145221b9291cf4225448117b
caio altmuller <150199353+Catitomio11@users.noreply.github.com>
2024-05-24 19:09:41 -0300

    Update L2.c

--- L2.c
+++ L2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+    char c;
+
+    // Solicita ao usuário para inserir uma string
+    printf("Digite um nome: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Conta o número de vogais na string
+    for(int i = 0; str[i] != '\0'; i++) {
+        c = tolower(str[i]);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            count++;
+        }
+    }
+
+    // Imprime o resultado
+    printf("O número de vogais no nome é: %d\n", count);
+
+    return 0;
+}
 
