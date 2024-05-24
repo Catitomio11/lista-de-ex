33bb87610d7cd025c0b029b3a3f6f9459c049795
caio altmuller <150199353+Catitomio11@users.noreply.github.com>
2024-05-24 19:51:08 -0300

    Update L8.c

--- L8.c
+++ L8.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Função para inverter um array utilizando ponteiros
+
 int* inverterArray(const int* arr, int tamanho) {
-    // Aloca memória para o novo array invertido
+
     int* arrayInvertido = (int*)malloc(tamanho * sizeof(int));
     if (arrayInvertido == NULL) {
         printf("Erro ao alocar memória.\n");
         exit(1);
     }
 
-    // Ponteiro para o final do array original
+
     const int* ptr = arr + tamanho - 1;
 
-    // Preenche o novo array invertido com os elementos do array original na ordem inversa
     for (int i = 0; i < tamanho; i++) {
         arrayInvertido[i] = *ptr;
         ptr--;
@@ -25,11 +24,10 @@ int* inverterArray(const int* arr, int tamanho) {
 int main() {
     int tamanho;
 
-    // Solicita ao usuário o tamanho do array
     printf("Digite o tamanho do array: ");
     scanf("%d", &tamanho);
 
-    // Solicita ao usuário para digitar os elementos do array
+
     int* arr = (int*)malloc(tamanho * sizeof(int));
     if (arr == NULL) {
         printf("Erro ao alocar memória.\n");
@@ -42,17 +40,17 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    // Chama a função para inverter o array
+
     int* arrayInvertido = inverterArray(arr, tamanho);
 
-    // Exibe o array invertido
+
     printf("Array Invertido:\n");
     for (int i = 0; i < tamanho; i++) {
         printf("%d ", arrayInvertido[i]);
     }
     printf("\n");
 
-    // Libera a memória alocada para os arrays
+
     free(arr);
     free(arrayInvertido);
 
