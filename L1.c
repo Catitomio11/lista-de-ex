087313515940cd1ce7e773deefdf82cf798c678e
caio altmuller <150199353+Catitomio11@users.noreply.github.com>
2024-05-24 19:07:15 -0300

    Update L1.c

--- L1.c
+++ L1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+void lerElementos(int *array, int tamanho) {
+    for(int i = 0; i < tamanho; i++) {
+        printf("Digite o elemento %d: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+}
+
+int somaElementos(int *array, int tamanho) {
+    int soma = 0;
+    for(int i = 0; i < tamanho; i++) {
+        soma += array[i];
+    }
+    return soma;
+}
+
+int main() {
+    int tamanho;
+
+    printf("Quantos elementos você deseja inserir? ");
+    scanf("%d", &tamanho);
+
+    int array[tamanho];
+
+    lerElementos(array, tamanho);
+
+    int soma = somaElementos(array, tamanho);
+
+    printf("A soma dos elementos dentro da array é: %d\n", soma);
+
+    return 0;
+}
