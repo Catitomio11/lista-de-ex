2167cb176103869fe0fdedc3700879031190ee19
caio altmuller <150199353+Catitomio11@users.noreply.github.com>
2024-05-24 19:47:17 -0300

    Update L10.c

--- L10.c
+++ L10.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+
+
+void processamentoSimples() {
+    for (int i = 0; i < 1000000; i++) {
+       
+        int resultado = i * i;
+    }
+}
+
+int main() {
+    
+    clock_t inicio, fim;
+    double tempoDecorrido;
+
+   
+    inicio = clock();
+
+
+    processamentoSimples();
+
+    
+    fim = clock();
+
+    
+    tempoDecorrido = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+
+ 
+    printf("Tempo decorrido: %.6f segundos\n", tempoDecorrido);
+
+    return 0;
+}
 
