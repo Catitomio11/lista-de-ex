5059765fdc2ab0aede34ade99e46415f799e7cf8
caio altmuller <150199353+Catitomio11@users.noreply.github.com>
2024-05-24 19:49:26 -0300

    Update L5.c

--- L5.c
+++ L5.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include <string.h>
 
-// Função para converter uma string para maiúsculas usando ponteiros
+
 void converterParaMaiusculas(char *texto) {
     while (*texto) {
         *texto = toupper(*texto);
@@ -13,20 +13,19 @@ void converterParaMaiusculas(char *texto) {
 int main() {
     char entrada[100];
 
-    // Solicita ao usuário para inserir uma string
+
     printf("Digite uma string: ");
     fgets(entrada, sizeof(entrada), stdin);
 
-    // Remove o caractere de nova linha lido por fgets
+   
     size_t comprimento = strlen(entrada);
     if (comprimento > 0 && entrada[comprimento - 1] == '\n') {
         entrada[comprimento - 1] = '\0';
     }
 
-    // Converte a string para maiúsculas
     converterParaMaiusculas(entrada);
 
-    // Exibe a string convertida
+
     printf("String convertida para maiúsculas: %s\n", entrada);
 
     return 0;
